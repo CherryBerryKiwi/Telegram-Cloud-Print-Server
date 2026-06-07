2487f7f343359f86a2bd592b810a6efe33cc5b90
Pham Hoang Thi <127835007+CherryBerryKiwi@users.noreply.github.com>
2026-06-07 12:35:00 +0700

    Update bot_secrets.example.py

--- bot_secrets.example.py
+++ bot_secrets.example.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "10987654321:ABCDEFGH"
-ALLOWED_CHAT_IDS: list[int] = [123456789]
+BOT_TOKEN = "10987654321:ABCDEFGH" #your telegram bot token (get from botfather)
+ALLOWED_CHAT_IDS: list[int] = [123456789] #only receive print commands from these IDs
